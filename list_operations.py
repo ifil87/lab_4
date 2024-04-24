e0562efaa0e4b42e778f23cfdcc38ff59e039034
ifil87 <filipchik2004@gmail.com>
2024-04-24 19:18:34 +0300

    add file

--- list_operations.py
+++ list_operations.py
@@ -21,4 +21,22 @@ print("Исходный список N:", N)
 # Замена значений элементов списка
 new_list = replace_element(N, K, M)
 print(f"Список после замены элементов со значением {K} на {M}: {new_list}")
+#6
 
+
+def digit_sum(number):
+    return sum(int(digit) for digit in str(abs(number)))
+
+
+def find_max_digit_sum_number(numbers):
+    max_number = numbers[0]
+    max_sum = digit_sum(numbers[0])
+
+    for num in numbers:
+        current_sum = digit_sum(num)
+        if current_sum > max_sum:
+            max_sum = current_sum
+            max_number = num
+
+    return max_number
+#7
